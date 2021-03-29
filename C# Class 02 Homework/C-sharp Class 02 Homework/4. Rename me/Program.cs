f44a3ee9e0a0746172ab47fec6c01b92bf615888
JanaSimjanovska <simjanovska.jana@gmail.com>
2021-03-29 13:03:33 +0200

    CSharp Class 02 Homework

---
+++ C# Class 02 Homework/C-sharp Class 02 Homework/4. Rename me/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _4._Rename_me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tik-tak, tik-tak...");
+
+            //Task 4:
+            //Write a program, where for a given number entered via the keyboard, it will print:
+
+
+            //"Tik" if divisible by 3,
+            //"Tak" if divisible by 5,
+            //"Tik - Tak" if divisible by 3 and 5.
+            //If the number is not divisible by 3 or 5, then print "Bad number".
+
+            Console.WriteLine("Enter a number: ");
+            bool isUserNumberSuccess = double.TryParse(Console.ReadLine(), out double userNumber);
+
+            if (isUserNumberSuccess)
+            {
+                if(userNumber % 3 == 0)
+                {
+                    if(userNumber % 5 == 0)
+                    {
+                        Console.WriteLine("Tik - Tak");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tik");
+                    }
+                    
+                }
+                else if(userNumber % 5 == 0)
+                {
+                    Console.WriteLine("Tak");
+                }
+                else
+                {
+                    Console.WriteLine("Bad number");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid integer!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Class 02 Homework/C-sharp Class 02 Homework/SwapNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's swap numbers :)");
+
+            //Task 3:
+            //SwapNumbers
+
+            Console.WriteLine("Enter first number (number should be an integer): ");
+            bool isUserFirstNumberSuccess = int.TryParse(Console.ReadLine(), out int userFirstNumber);
+            Console.WriteLine("Enter second number (number should be an integer): ");
+            bool isUserSecondNumberSuccess = int.TryParse(Console.ReadLine(), out int userSecondNumber);
+
+            Console.WriteLine(userFirstNumber);
+            Console.WriteLine(userSecondNumber);
+
+            int tempVar1 = userFirstNumber;
+            int tempVar2 = userSecondNumber;
+
+            userFirstNumber = tempVar2;
+            userSecondNumber = tempVar1;
+
+
+            if(isUserFirstNumberSuccess && isUserSecondNumberSuccess)
+            {
+                Console.WriteLine("First number: " + userFirstNumber + " \r\nSecond number: " + userSecondNumber);
+            }
+            else
+            {
+                Console.WriteLine("Please enter valid integers!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
